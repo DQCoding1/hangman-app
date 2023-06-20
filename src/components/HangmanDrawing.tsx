ab7cd9d003c881e725376515f876b9cad3ca93a9
DQCoding1 <diegoabquintero@gmail.com>
2023-06-20 17:24:51 -0500

    hangman body

--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -1,5 +1,26 @@
 import "./HangmanDrawing.css"
 
+const bodyparts = {
+  head: (
+    <div className="structure__head"/>
+  ),
+  body:  (
+    <div className="structure__body"/>
+  ), 
+  rightArm: (
+    <div className="structure__rightArm"/>
+  ),
+  leftArm: (
+    <div className="structure__leftArm"/>
+  ),
+  rightleg:(
+    <div className="structure__rightleg"/>  
+  ),
+  leftleg: (
+    <div className="structure__leftleg"/>  
+  )
+}
+
 const HangmanDrawing = () => {
   return (
     <div className="structure">
@@ -7,6 +28,14 @@ const HangmanDrawing = () => {
       <div className="structure__hangBar"></div>
       <div className="structure__middleBar"></div>
       <div className="structure__bottomBar"></div>
+      <div className="structure__bodyparts">
+        {bodyparts.head}
+        {bodyparts.body}
+        {bodyparts.rightArm}
+        {bodyparts.leftArm}
+        {bodyparts.rightleg}
+        {bodyparts.leftleg}
+      </div>
     </div>
   )
 }
