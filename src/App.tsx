dc5ea5d8ff13d0b80722894cec65fc1a23c7bf64
DQCoding1 <diegoabquintero@gmail.com>
2023-06-21 16:54:30 -0500

    implementing screen keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,52 @@
-import './App.css'
-import { useEffect, useState } from "react"
-import { wordList } from './data/wordList'
-import HangmanDrawing from './components/HangmanDrawing'
-import HangmanWord from './components/HangmanWord'
-import Keyboard from './components/Keyboard'
-
+import "./App.css";
+import { useEffect, useState } from "react";
+import { wordList } from "./data/wordList";
+import HangmanDrawing from "./components/HangmanDrawing";
+import HangmanWord from "./components/HangmanWord";
+import Keyboard from "./components/Keyboard";
 
 function App() {
   const [wordToFind] = useState(() => {
-    return wordList[Math.round(Math.random() * wordList.length)]
-  })
-  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
-  const incorrectLetters = guessedLetters.filter(item => !wordToFind.includes(item))
+    return wordList[Math.round(Math.random() * wordList.length)];
+  });
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+  const incorrectLetters = guessedLetters.filter(
+    (item) => !wordToFind.includes(item)
+  );
 
-
-  const addGuessedLetter = (key : string) => {
-    if (guessedLetters.includes(key)) return 
-
-    setGuessedLetters(currentLetters => [...currentLetters, key])
-  }
-
+  const addGuessedLetter = (key: string) => {
+    if (guessedLetters.includes(key)) return;
+    setGuessedLetters((currentLetters) => [...currentLetters, key]);
+  };
 
   useEffect(() => {
     const handler = (e: KeyboardEvent) => {
-      const key = e.key
+      const key = e.key;
 
-      if (!key.match(/^[a-z]$/)) return 
+      if (!key.match(/^[a-z]$/)) return;
 
-      e.preventDefault()
-      addGuessedLetter(key)
-    }
+      e.preventDefault();
+      addGuessedLetter(key);
+    };
 
-    document.addEventListener("keypress", handler)
+    document.addEventListener("keypress", handler);
 
     return () => {
-      document.removeEventListener("keypress", handler)
-    }
-  }, [guessedLetters])
+      document.removeEventListener("keypress", handler);
+    };
+  }, [guessedLetters]);
 
-  
   return (
-    <main className='app'>
-      <HangmanDrawing numberOfIncorrectGuesses={incorrectLetters.length}/>
-      <HangmanWord guessedLetters={guessedLetters} wordToFind={wordToFind}/>
-      <Keyboard />
+    <main className="app">
+      <HangmanDrawing numberOfIncorrectGuesses={incorrectLetters.length} />
+      <HangmanWord guessedLetters={guessedLetters} wordToFind={wordToFind} />
+      <Keyboard
+        incorrectLetter={incorrectLetters}
+        correctLetters={guessedLetters.filter(item => wordToFind.includes(item))}
+        addGuesedLetters={addGuessedLetter}
+      />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,16 +1,39 @@
-import { KEYS } from "../data/alphabet"
-import "./Keyboard.css"
+import { KEYS } from "../data/alphabet";
+import "./Keyboard.css";
 
-const Keyboard = () => {
+type KeyboardProps = {
+  incorrectLetter: string[];
+  correctLetters: string[];
+  addGuesedLetters: (key: string) => void;
+};
+const Keyboard = ({
+  incorrectLetter,
+  correctLetters,
+  addGuesedLetters,
+}: KeyboardProps) => {
   return (
     <ul className="keyboard">
-      {KEYS.map((item, index) => (
-        <li className="keyboard__li--incorrect">
-          <button key={index} className="keyboard__button">{item}</button>
-        </li>
-      ))}
+      {KEYS.map((item, index) => {
+        const isLetterCorrect = correctLetters.includes(item)
+        const isLetterIncorrect = incorrectLetter.includes(item)
+        return (
+          <li
+            className={`keyboard__li 
+            ${isLetterCorrect  ? "keyboard__li--correct" : ""} 
+            ${ isLetterIncorrect ? "keyboard__li--incorrect" : ""}`}
+          >
+            <button
+              key={index}
+              onClick={() => addGuesedLetters(item)}
+              className="keyboard__button"
+              disabled={isLetterCorrect || isLetterIncorrect}
+            >
+              {item}
+            </button>
+          </li>
+      )})}
     </ul>
-  )
-}
+  );
+};
 
-export default Keyboard
+export default Keyboard;
