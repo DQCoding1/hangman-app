09bce983b983009468910d47e16f15ce2b87c774
DQCoding1 <diegoabquintero@gmail.com>
2023-06-21 14:39:40 -0500

    implementing computer keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { wordList } from './data/wordList'
 import HangmanDrawing from './components/HangmanDrawing'
 import HangmanWord from './components/HangmanWord'
@@ -7,13 +7,38 @@ import Keyboard from './components/Keyboard'
 
 
 function App() {
-  const [wordToFind, setWordToFind] = useState(() => {
+  const [wordToFind] = useState(() => {
     return wordList[Math.round(Math.random() * wordList.length)]
   })
-
   const [guessedLetters, setGuessedLetters] = useState<string[]>([])
   const incorrectLetters = guessedLetters.filter(item => !wordToFind.includes(item))
 
+
+  const addGuessedLetter = (key : string) => {
+    if (guessedLetters.includes(key)) return 
+
+    setGuessedLetters(currentLetters => [...currentLetters, key])
+  }
+
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key
+
+      if (!key.match(/^[a-z]$/)) return 
+
+      e.preventDefault()
+      addGuessedLetter(key)
+    }
+
+    document.addEventListener("keypress", handler)
+
+    return () => {
+      document.removeEventListener("keypress", handler)
+    }
+  }, [guessedLetters])
+
+  
   return (
     <main className='app'>
       <HangmanDrawing numberOfIncorrectGuesses={incorrectLetters.length}/>
